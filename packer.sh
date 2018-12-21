480e64d06a38cdd6ecf7a336ff4ee8bd5cd6c366
javier_marques <javier.marques.ruiz@gmail.com>
2018-12-21 20:15:51 +0100

    added packer to build an image

---
+++ packer.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+GOOS=linux go build -ldflags "-X main.GitCommit=$TRAVIS_COMMIT" -o server-linux
+
+wget https://releases.hashicorp.com/packer/1.3.3/packer_1.3.3_linux_amd64.zip
+unzip packer_1.3.3_linux_amd64.zip
+./packer build packer/server.json
