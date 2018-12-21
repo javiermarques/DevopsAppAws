d31bfff2422fb97bdd4f9d84eb8927d6fa1d72c5
javier_marques <javier.marques.ruiz@gmail.com>
2018-12-21 12:57:35 +0100

    Added version endpoint

--- server.go
+++ server.go
@@ -6,12 +6,24 @@ import (
 	"github.com/labstack/echo"
 )
 
+// Version application number
+const Version = "0.1"
+
+// GitCommit hash linked var in build time
+var GitCommit string
+
 func main() {
 	e := echo.New()
 	e.GET("/", helloWorld)
+	e.GET("/version", version)
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
 func helloWorld(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, World!")
+}
+
+func version(c echo.Context) error {
+
+	return c.HTML(http.StatusOK, "Version: "+Version+"</br>Hash: "+GitCommit)
 }
