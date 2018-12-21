083731768155c1a94a7006bafbd652a9776d6a9c
javier_marques <javier.marques.ruiz@gmail.com>
2018-12-21 20:52:41 +0100

    fix template route

--- packer.sh
+++ packer.sh
@@ -3,4 +3,4 @@ GOOS=linux go build -ldflags "-X main.GitCommit=$TRAVIS_COMMIT" -o server-linux
 
 wget https://releases.hashicorp.com/packer/1.3.3/packer_1.3.3_linux_amd64.zip
 unzip -o packer_1.3.3_linux_amd64.zip
-./packer build packer/server.json
+./packer build packer-template/server.json
