d0a4ea53871bb3d485053d38b7276504a7f71ced
javier_marques <javier.marques.ruiz@gmail.com>
2018-12-22 16:34:46 +0100

    added GOARCH env var

--- packer.sh
+++ packer.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-GOOS=linux go build -ldflags "-X main.GitCommit=$TRAVIS_COMMIT" -o server-linux
+
+GOOS=linux GOARCH=amd64 go build -ldflags "-X main.GitCommit=$TRAVIS_COMMIT" -o server-linux
+echo "Build Done"
 
 wget https://releases.hashicorp.com/packer/1.3.3/packer_1.3.3_linux_amd64.zip
 unzip -o packer_1.3.3_linux_amd64.zip
