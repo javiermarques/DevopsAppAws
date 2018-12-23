e8337e7352aec8d72207857825ef99851f95d3bd
javier_marques <javier.marques.ruiz@gmail.com>
2018-12-23 21:37:57 +0100

    added more flags to the build

--- packer.sh
+++ packer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-GOOS=linux GOARCH=amd64 go build -ldflags "-X main.GitCommit=$TRAVIS_COMMIT" -o server-linux
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -installsuffix cgo -ldflags "-w -s -X main.GitCommit=$TRAVIS_COMMIT" -o server-linux
 echo "Build Done"
 
 wget https://releases.hashicorp.com/packer/1.3.3/packer_1.3.3_linux_amd64.zip
