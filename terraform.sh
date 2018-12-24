fe1ca91c0a5ddfa4c0683ca935f9b2b08d0c3bb7
javier_marques <javier.marques.ruiz@gmail.com>
2018-12-24 17:15:29 +0100

    added terraform init

--- terraform.sh
+++ terraform.sh
@@ -2,4 +2,5 @@
 cd tf
 wget https://releases.hashicorp.com/terraform/0.11.11/terraform_0.11.11_linux_amd64.zip
 unzip -o terraform_0.11.11_linux_amd64.zip
+./terraform init
 ./terraform apply -auto-approve
