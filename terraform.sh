4c5393b7549a54a625c168150b7ad4ca4ec81798
javier_marques <javier.marques.ruiz@gmail.com>
2018-12-24 17:00:29 +0100

    moving to tf directory first

--- terraform.sh
+++ terraform.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
+cd tf
 wget https://releases.hashicorp.com/terraform/0.11.11/terraform_0.11.11_linux_amd64.zip
 unzip -o terraform_0.11.11_linux_amd64.zip
-cd tf
 ./terraform apply -auto-approve
